0e56b9f40994241f472fd1f7b2ad2dc86265940f
PKPrasantaBehera <106430949+PKPrasantaBehera@users.noreply.github.com>
2024-11-07 19:10:30 +0530

    Add files via upload

---
+++ Data/BikeStores  Database - drop all objects.sql
@@ -0,0 +1,26 @@
+/*
+--------------------------------------------------------------------
+© 2017 sqlservertutorial.net All Rights Reserved
+--------------------------------------------------------------------
+Name   : BikeStores
+Link   : http://www.sqlservertutorial.net/load-sample-database/
+Version: 1.0
+--------------------------------------------------------------------
+*/
+
+-- drop tables
+DROP TABLE sales.order_items;
+DROP TABLE sales.orders;
+DROP TABLE production.stocks;
+DROP TABLE production.products;
+DROP TABLE  production.categories;
+DROP TABLE  production.brands;
+DROP TABLE   sales.customers;
+DROP TABLE  sales.staffs;
+DROP TABLE  sales.stores;
+
+-- drop the schemas
+
+DROP SCHEMA sales;
+DROP SCHEMA  production;
+
